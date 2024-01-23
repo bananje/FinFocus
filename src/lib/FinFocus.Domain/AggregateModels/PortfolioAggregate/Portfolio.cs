43ceb40c30506ca7495a9d4e6c044edd8adf358f
Hermann Kenig <vgerman2004@mail.ru>
2024-01-23 09:32:43 +0300

    'PortfolioReview'

--- src/lib/FinFocus.Domain/AggregateModels/PortfolioAggregate/Portfolio.cs
+++ src/lib/FinFocus.Domain/AggregateModels/PortfolioAggregate/Portfolio.cs
@@ -1,12 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FinFocus.Domain.EntitieModels.Common;
 
 namespace FinFocus.Domain.AggregateModels.PortfolioAggregate
 {
     internal class Portfolio
     {
+        private readonly HashSet<IAsset> _assets = new();
+
+        /// <summary>
+        /// Наименование портфеля
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Дата создания портфеля
+        /// </summary>
+        public DateTime CreatedDate { get; set; }
+
+        /// <summary>
+        /// Общая стоимость 
+        /// </summary>
+        public decimal TotalPrice { get; set; }
+
+        /// <summary>
+        /// Процент акций в портфеле
+        /// </summary>
+        public decimal SharesPercent { get; set; }
+
+        /// <summary>
+        /// Процент облигаций в порфтеле
+        /// </summary>
+        public decimal BoundsPercent { get; set; }
+
+        /// <summary>
+        /// Процент EFT в портфеле
+        /// </summary>
+        public decimal EtfPercent { get; set; }
+
+        /// <summary>
+        /// Процент за год
+        /// </summary>
+        public decimal PercentPerYear { get; set; }
+
+        /// <summary>
+        /// Расчётный потенциал
+        /// </summary>
+        public decimal Potential { get; set; }
+
+        /// <summary>
+        /// Средний P/E активов в портфеле
+        /// </summary>
+        public double PeAvg { get; set; }
+
+        /// <summary>
+        /// Активы в портфеле
+        /// </summary>
+        public IReadOnlyCollection<IAsset> Assets => _assets;
+
+        public Portfolio() { }
     }
 }
